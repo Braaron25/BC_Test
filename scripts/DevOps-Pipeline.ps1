272d88c8412786cc2061dcc9216d6a919377b5a4
Brandon Romero <bromero@altioracorp.com>
2025-01-27 12:37:12 -0500

    Update pipeline script

--- scripts/DevOps-Pipeline.ps1
+++ scripts/DevOps-Pipeline.ps1
@@ -10,65 +10,6 @@
     [int] $appRevision = 0
 )
 
-Write-Host "info=$ENV:ClientId"
-
-if ($environment -eq "AzureDevOps") {
-    Write-Host "azure: $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY"
-    $buildArtifactFolder = $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY
-}
-
-# Installar BcContainerHelper
-# Install-Module BcContainerHelper -Force
-
-$baseFolder = (Get-Item (Join-Path $PSScriptRoot "..")).FullName
-Write-Host "Base folder: $baseFolder"
-. (Join-Path $PSScriptRoot "Read-Settings.ps1") -environment $environment -version $version
-# . (Join-Path $PSScriptRoot "Install-BcContainerHelper.ps1") -bcContainerHelperVersion $bcContainerHelperVersion -genericImageName $genericImageName
-
-$clientID = $ENV:ClientId
-$clientSecret = $ENV:ClientSecret
-$tenantId = $ENV:TenantId
-
-$authContext = New-BcAuthContext `
-    -clientID $clientID `
-    -clientSecret $clientSecret `
-    -tenantID $tenantId `
-    -scopes "https://api.businesscentral.dynamics.com/.default" `
-    -includeDeviceLogin
-
-# $environment = Get-BcEnvironments -bcAuthContext $authContext -environment 'DEV-ALT'
-
-$environmentName = $ENV:BCEnvironment
-Write-Host "Env: $environmentName"
-
-Start-Sleep -Seconds 10
-Write-Host "Searching test artifact"
-$currentApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Id -eq "c0f2cb46-b96d-4e7d-a45d-c8ec60d7f19f" }
-
-if($currentApp){
-    $currentApp | Out-Host
-    SearchAppJson
-} else {
-    RunPipeline
-}
-
-function SearchAppJson {
-    $path = Join-Path $PSScriptRoot ".."
-
-Get-ChildItem -Path $path | Where-Object { $_.PSIsContainer -and $_.Name -notlike ".*" } | Get-ChildItem -Recurse -Filter "app.json" | ForEach-Object {
-    $appJsonFile = $_.FullName
-    $appJson = Get-Content $appJsonFile | ConvertFrom-Json
-    # Write-Host "JSON: $appJson"
-    Write-Host "Current version"
-    if(!($currentApp.Version -eq $appJson.version)){
-        RunPipeline
-    }else{
-        Write-Host "Ya posee la ultima version"
-    }
-}
-}
-
-
 function RunPipeline {
     do {
         Start-Sleep -Seconds 10
@@ -154,4 +95,61 @@ function RunPipeline {
     }
 }
 
+function SearchAppJson {
+    $path = Join-Path $PSScriptRoot ".."
 
+    Get-ChildItem -Path $path | Where-Object { $_.PSIsContainer -and $_.Name -notlike ".*" } | Get-ChildItem -Recurse -Filter "app.json" | ForEach-Object {
+        $appJsonFile = $_.FullName
+        $appJson = Get-Content $appJsonFile | ConvertFrom-Json
+        # Write-Host "JSON: $appJson"
+        Write-Host "Current version"
+        if(!($currentApp.Version -eq $appJson.version)){
+            RunPipeline
+        }else{
+            Write-Host "Ya posee la ultima version"
+        }
+    }
+}
+
+
+Write-Host "info=$ENV:ClientId"
+
+if ($environment -eq "AzureDevOps") {
+    Write-Host "azure: $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY"
+    $buildArtifactFolder = $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY
+}
+
+# Installar BcContainerHelper
+# Install-Module BcContainerHelper -Force
+
+$baseFolder = (Get-Item (Join-Path $PSScriptRoot "..")).FullName
+Write-Host "Base folder: $baseFolder"
+. (Join-Path $PSScriptRoot "Read-Settings.ps1") -environment $environment -version $version
+# . (Join-Path $PSScriptRoot "Install-BcContainerHelper.ps1") -bcContainerHelperVersion $bcContainerHelperVersion -genericImageName $genericImageName
+
+$clientID = $ENV:ClientId
+$clientSecret = $ENV:ClientSecret
+$tenantId = $ENV:TenantId
+
+$authContext = New-BcAuthContext `
+    -clientID $clientID `
+    -clientSecret $clientSecret `
+    -tenantID $tenantId `
+    -scopes "https://api.businesscentral.dynamics.com/.default" `
+    -includeDeviceLogin
+
+# $environment = Get-BcEnvironments -bcAuthContext $authContext -environment 'DEV-ALT'
+
+$environmentName = $ENV:BCEnvironment
+Write-Host "Env: $environmentName"
+
+Start-Sleep -Seconds 10
+Write-Host "Searching test artifact"
+$currentApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Id -eq "c0f2cb46-b96d-4e7d-a45d-c8ec60d7f19f" }
+
+if($currentApp){
+    $currentApp | Out-Host
+    SearchAppJson
+} else {
+    RunPipeline
+}
