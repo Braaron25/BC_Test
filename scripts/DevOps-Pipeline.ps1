1f5794a5f76f5d4c9fd4e7327fba53496d4c5896
Brandon Romero <bromero@altioracorp.com>
2025-01-27 12:13:27 -0500

    Validate version test

--- scripts/DevOps-Pipeline.ps1
+++ scripts/DevOps-Pipeline.ps1
@@ -43,84 +43,107 @@ Write-Host "Env: $environmentName"
 
 do {
     Start-Sleep -Seconds 10
-    $baseApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Name -eq "Base Application" }
-} while (!($baseApp))
-$baseapp | Out-Host
+    $currentApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Name -eq "Test_Pro" }
+} while (!($currentApp))
+$currentApp | Out-Host
 
-$artifact = Get-BCArtifactUrl `
-    -country 'us' `
-    -version $baseApp.Version `
-    -select Closest
+$path = Join-Path $PSScriptRoot ".."
+
+Get-ChildItem -Path $path | Where-Object { $_.PSIsContainer -and $_.Name -notlike ".*" } | Get-ChildItem -Recurse -Filter "app.json" | ForEach-Object {
+    $appJsonFile = $_.FullName
+    $appJson = Get-Content $appJsonFile | ConvertFrom-Json
+    # Write-Host "JSON: $appJson"
+    Write-Host "Current version"
+    if(!($currentApp.Version -eq $appJson.version)){
+        RunPipeline
+    }else{
+        Write-Host "Ya posee la ultima version"
+    }
+}
+
+function RunPipeline {
+    do {
+        Start-Sleep -Seconds 10
+        $baseApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Name -eq "Base Application" }
+    } while (!($baseApp))
+    $baseapp | Out-Host
     
-if ($artifact) {
-    Write-Host "Using Artifacts: $artifact"
+    $artifact = Get-BCArtifactUrl `
+        -country 'us' `
+        -version $baseApp.Version `
+        -select Closest
+        
+    if ($artifact) {
+        Write-Host "Using Artifacts: $artifact"
+    }
+    else {
+        throw "No artifacts available"
+    }
+    
+    # $params = @{}
+    # $insiderSasToken = "$ENV:insiderSasToken"
+    # $licenseFile = "$ENV:licenseFile"
+    # $codeSigncertPfxFile = "$ENV:CodeSignCertPfxFile"
+    # if (!$doNotSignApps -and $codeSigncertPfxFile) {
+    #     if ("$ENV:CodeSignCertPfxPassword" -ne "") {
+    #         $codeSignCertPfxPassword = try { "$ENV:CodeSignCertPfxPassword" | ConvertTo-SecureString } catch { ConvertTo-SecureString -String "$ENV:CodeSignCertPfxPassword" -AsPlainText -Force }
+    #         $params = @{
+    #             "codeSignCertPfxFile"     = $codeSignCertPfxFile
+    #             "codeSignCertPfxPassword" = $codeSignCertPfxPassword
+    #         }
+    #     }
+    #     else {
+    #         $codeSignCertPfxPassword = $null
+    #     }
+    # }
+    
+    $allTestResults = "testresults*.xml"
+    $testResultsFile = Join-Path $baseFolder "TestResults.xml"
+    $testResultsFiles = Join-Path $baseFolder $allTestResults
+    if (Test-Path $testResultsFiles) {
+        Remove-Item $testResultsFiles -Force
+    }
+    
+    Write-Host "param=$params"
+    
+    Run-AlPipeline `
+        -pipelinename $pipelineName `
+        -containerName $containerName `
+        -imageName $imageName `
+        -bcAuthContext $authContext `
+        -environment $environmentName `
+        -artifact $artifact `
+        -memoryLimit $memoryLimit `
+        -baseFolder $baseFolder `
+        -installApps $installApps `
+        -installTestApps $installTestApps `
+        -previousApps $previousApps `
+        -appFolders $appFolders `
+        -testFolders $testFolders `
+        -doNotRunTests:$doNotRunTests `
+        -testResultsFile $testResultsFile `
+        -testResultsFormat 'JUnit' `
+        -installTestRunner:$installTestRunner `
+        -installTestFramework:$installTestFramework `
+        -installTestLibraries:$installTestLibraries `
+        -installPerformanceToolkit:$installPerformanceToolkit `
+        -enableCodeCop:$enableCodeCop `
+        -enableAppSourceCop:$enableAppSourceCop `
+        -enablePerTenantExtensionCop:$enablePerTenantExtensionCop `
+        -enableUICop:$enableUICop `
+        -azureDevOps:($environment -eq 'AzureDevOps') `
+        -gitLab:($environment -eq 'GitLab') `
+        -gitHubActions:($environment -eq 'GitHubActions') `
+        -failOn 'error' `
+        -AppSourceCopMandatoryAffixes $appSourceCopMandatoryAffixes `
+        -AppSourceCopSupportedCountries $appSourceCopSupportedCountries `
+        -additionalCountries $additionalCountries `
+        -buildArtifactFolder $buildArtifactFolder
+        # -appBuild $appBuild -appRevision $appRevision
+    
+    if ($environment -eq 'AzureDevOps') {
+        Write-Host "##vso[task.setvariable variable=TestResults]$allTestResults"
+    }
 }
-else {
-    throw "No artifacts available"
-}
 
-# $params = @{}
-# $insiderSasToken = "$ENV:insiderSasToken"
-# $licenseFile = "$ENV:licenseFile"
-# $codeSigncertPfxFile = "$ENV:CodeSignCertPfxFile"
-# if (!$doNotSignApps -and $codeSigncertPfxFile) {
-#     if ("$ENV:CodeSignCertPfxPassword" -ne "") {
-#         $codeSignCertPfxPassword = try { "$ENV:CodeSignCertPfxPassword" | ConvertTo-SecureString } catch { ConvertTo-SecureString -String "$ENV:CodeSignCertPfxPassword" -AsPlainText -Force }
-#         $params = @{
-#             "codeSignCertPfxFile"     = $codeSignCertPfxFile
-#             "codeSignCertPfxPassword" = $codeSignCertPfxPassword
-#         }
-#     }
-#     else {
-#         $codeSignCertPfxPassword = $null
-#     }
-# }
-
-$allTestResults = "testresults*.xml"
-$testResultsFile = Join-Path $baseFolder "TestResults.xml"
-$testResultsFiles = Join-Path $baseFolder $allTestResults
-if (Test-Path $testResultsFiles) {
-    Remove-Item $testResultsFiles -Force
-}
-
-Write-Host "param=$params"
-
-Run-AlPipeline `
-    -pipelinename $pipelineName `
-    -containerName $containerName `
-    -imageName $imageName `
-    -bcAuthContext $authContext `
-    -environment $environmentName `
-    -artifact $artifact `
-    -memoryLimit $memoryLimit `
-    -baseFolder $baseFolder `
-    -installApps $installApps `
-    -installTestApps $installTestApps `
-    -previousApps $previousApps `
-    -appFolders $appFolders `
-    -testFolders $testFolders `
-    -doNotRunTests:$doNotRunTests `
-    -testResultsFile $testResultsFile `
-    -testResultsFormat 'JUnit' `
-    -installTestRunner:$installTestRunner `
-    -installTestFramework:$installTestFramework `
-    -installTestLibraries:$installTestLibraries `
-    -installPerformanceToolkit:$installPerformanceToolkit `
-    -enableCodeCop:$enableCodeCop `
-    -enableAppSourceCop:$enableAppSourceCop `
-    -enablePerTenantExtensionCop:$enablePerTenantExtensionCop `
-    -enableUICop:$enableUICop `
-    -azureDevOps:($environment -eq 'AzureDevOps') `
-    -gitLab:($environment -eq 'GitLab') `
-    -gitHubActions:($environment -eq 'GitHubActions') `
-    -failOn 'error' `
-    -AppSourceCopMandatoryAffixes $appSourceCopMandatoryAffixes `
-    -AppSourceCopSupportedCountries $appSourceCopSupportedCountries `
-    -additionalCountries $additionalCountries `
-    -buildArtifactFolder $buildArtifactFolder
-    # -appBuild $appBuild -appRevision $appRevision
-
-if ($environment -eq 'AzureDevOps') {
-    Write-Host "##vso[task.setvariable variable=TestResults]$allTestResults"
-}
 
