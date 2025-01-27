29e5363daa42ac4ed6428d625ca110696ceb4848
Brandon Romero <bromero@altioracorp.com>
2025-01-27 09:41:39 -0500

    update artifact path on production scipt

--- scripts/Publish-to-Production.ps1
+++ scripts/Publish-to-Production.ps1
@@ -4,4 +4,4 @@ Publish-PerTenantExtensionApps -useNewLine `
  -ClientSecret $ENV:ClientSecret `
  -tenantId $ENV:TenantId `
  -environment $ENV:ProEnv `
- -appFiles @(Get-Item "Artifacts/output/Apps/*.app" | % { $_.FullName })
+ -appFiles @(Get-Item ".output/*.app" | % { $_.FullName })
