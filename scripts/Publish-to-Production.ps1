8746b56c9b5be8b8bb95a9be1a7ed9f451e5584b
Brandon Romero <bromero@altioracorp.com>
2025-06-05 10:35:05 -0500

    Actualizacion para GitHub Actions

--- scripts/Publish-to-Production.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Publishing to tenant"
-Publish-PerTenantExtensionApps -useNewLine `
- -ClientID $ENV:ClientId `
- -ClientSecret $ENV:ClientSecret `
- -tenantId $ENV:TenantId `
- -environment $ENV:ProEnv `
- -schemaSyncMode 'Force' `
- -appFiles @(Get-Item ".output/*.app" | % { $_.FullName })
